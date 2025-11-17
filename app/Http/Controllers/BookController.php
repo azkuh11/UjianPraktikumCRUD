9a5b66387a87cea816425eb96519093b7ea124e6
azkuh11 <azkuh11@gmail.com>
2025-11-17 08:59:23 +0700

    remove comments

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -8,26 +8,17 @@ use Illuminate\Support\Facades\Storage;
 
 class BookController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $books = Book::all();
         return view('books.index', compact('books'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         return view('books.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         $validated = $request->validate([
@@ -50,27 +41,18 @@ class BookController extends Controller
         return redirect()->route('books.index')->with('success', 'Buku berhasil ditambahkan!');
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
         $book = Book::findOrFail($id);
         return view('books.show', compact('book'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(string $id)
     {
         $book = Book::findOrFail($id);
         return view('books.edit', compact('book'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
         $book = Book::findOrFail($id);
@@ -84,18 +66,16 @@ class BookController extends Controller
         ]);
 
         if ($request->hasFile('cover_image')) {
-            // Hapus file lama jika ada
+
             if ($book->cover_image && Storage::exists('public/covers/' . $book->cover_image)) {
                 Storage::delete('public/covers/' . $book->cover_image);
             }
 
-            // Simpan file baru
             $file = $request->file('cover_image');
             $fileName = time() . '_' . $file->getClientOriginalName();
             Storage::putFileAs('public/covers', $file, $fileName);
             $validated['cover_image'] = $fileName;
         } else {
-            // Jika tidak upload gambar baru, tetap gunakan gambar lama
             $validated['cover_image'] = $book->cover_image;
         }
 
@@ -104,14 +84,10 @@ class BookController extends Controller
         return redirect()->route('books.index')->with('success', 'Buku berhasil diperbarui!');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
         $book = Book::findOrFail($id);
 
-        // Hapus file cover_image jika ada
         if ($book->cover_image && Storage::exists('public/covers/' . $book->cover_image)) {
             Storage::delete('public/covers/' . $book->cover_image);
         }
